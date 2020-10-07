5d68eee739e6c216fd9348efe9816ab84947af40
Ishtiak Ahmed Fuad <light@pop-os.localdomain>
2020-10-07 19:20:43 +0600

    added form

--- tweets/views.py
+++ tweets/views.py
@@ -1,15 +1,27 @@
+import random
 from django.http import HttpResponse, Http404,JsonResponse
 from django.shortcuts import render
 
+from .forms import TweetForm
 from .models import Tweet
 
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
 
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        # do other form related logic
+        obj.save()
+        form = TweetForm()
+    return render(request, 'components/form.html', context={"form": form})
+
+
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 100)} for x in qs]
     data = {
         "isUser": False,
         "response": tweets_list
